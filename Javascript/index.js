535b10fb2a6c4cae92a1d7a98d87228eb72ffe34
parinyad123 <sci.equ@gmail.com>
2020-08-09 17:47:32 +0700

    Build Buttom

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,2 +1,12 @@
-console.log('Good bye World');
+let content2 = document.getElementById('content-2'); // ทำให้ html ใช้ js ได้ ด้วย doc..getE..
+let textHtml = '<b>MacOS</b>';
+content2.innerHTML = textHtml;
 
+let discountButton = document.getElementById('discount-button');
+let message = document.getElementById('message');
+function showMessage() {
+    message.innerHTML = 'Time out';
+}
+// ทำให้ปุ่มทำงาน addEventListener(เหตุการณ์ , คำสั่ง)
+// ไม่่ใช่ () ที่ showMessage  เพราะไม่ต้องการให้้ทำงานเลย จะทำงานเมื่อปุ่มถูกกด
+discountButton.addEventListener('click',showMessage);
